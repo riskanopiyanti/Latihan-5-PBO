a7c9cbb98aba563361c33a5ac06fae871494b75c
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-03-15 14:16:25 +0700

    Create Right triangle.java

---
+++ Right triangle.java
@@ -0,0 +1,35 @@
+/* Dynamic polymorphism using Overriding method, Run-time binding/late binding */
+public class RightTriangle extends Triangle {
+    private Double base;
+    private Double height;
+
+    public RightTriangle(String name, Double base, Double height) {
+        super();
+        this.name = name;
+        this.base = base;
+        this.height = height;
+        this.calculateArea();
+        super.side1 = base;
+        super.side2 = height;
+        super.side3 = this.getHyptenuse();
+    }
+
+    private double getHyptenuse() {
+        return Math.sqrt((base * base) + (height * height));
+    }
+
+    public void calculateArea() {
+        super.area = (base * height) / 2;
+    }
+
+    public void getInfo() {
+        System.out.print("Right Triangle " + name + " info ");
+        System.out.print(" area: " + super.area);
+        System.out.print(" with base: " + base);
+        System.out.print(" and height: " + height);
+        System.out.print(" and hypotenuse: " + this.getHyptenuse());
+        System.out.println();
+    }
+    void print() {
+    }  
+}
